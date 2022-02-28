cf2ae556cb48e7acda48736a818314fc9422d9a8
kodluyoruz <baristutakli@hotmail.com>
2022-02-28 19:24:43 +0300

    TagViewModels

---
+++ BarisTutakli.Blog.WebAPI/Application/ViewModels/TagViewModels/CreateTagModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BarisTutakli.Blog.WebAPI.Application.ViewModels.TagViewModels
+{
+    public class CreateTagModel
+    {
+        public string Title { get; set; }
+        public string MetaTitle { get; set; }
+        public string Contents { get; set; }
+    }
+}
