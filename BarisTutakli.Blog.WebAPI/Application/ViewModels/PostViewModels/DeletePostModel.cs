b8d2893f810372050b9f09d82d63350ffa42aee4
kodluyoruz <baristutakli@hotmail.com>
2022-02-28 19:25:22 +0300

    PostViewModels

---
+++ BarisTutakli.Blog.WebAPI/Application/ViewModels/PostViewModels/DeletePostModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BarisTutakli.Blog.WebAPI.Application.ViewModels.PostViewModels
+{
+    [NotMapped]
+    public class DeletePostModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ BarisTutakli.Blog.WebAPI/Application/ViewModels/UserViewModels/CreateUserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace BarisTutakli.Blog.WebAPI.Application.ViewModels.UserViewModels
+{
+    public class CreateUserModel
+    {
+
+        [Required(ErrorMessage = "User Name is required")]
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8), MaxLength(20)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
