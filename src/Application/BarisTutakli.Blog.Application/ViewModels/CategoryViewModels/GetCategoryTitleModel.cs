2c2c22532cf594eb7e0f5c5da117e8cf7db54196
kodluyoruz <baristutakli@hotmail.com>
2022-03-01 12:11:39 +0300

    Category and Comment view models

---
+++ src/Application/BarisTutakli.Blog.Application/ViewModels/CategoryViewModels/GetCategoryTitleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BarisTutakli.Blog.Application.ViewModels.CategoryViewModels
+{
+    [NotMapped]
+    public class GetCategoryTitleModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ src/Application/BarisTutakli.Blog.Application/ViewModels/CommentViewModels/UpdateCommentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BarisTutakli.Blog.Application.ViewModels.CommentViewModels
+{
+    public class UpdateCommentModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Published { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string Body { get; set; }
+    }
+}
