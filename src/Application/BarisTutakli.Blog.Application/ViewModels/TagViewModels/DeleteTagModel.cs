95961a1ac57fa165b55f38132a82e7b601534723
kodluyoruz <baristutakli@hotmail.com>
2022-03-01 12:10:38 +0300

    TagViewModels

---
+++ src/Application/BarisTutakli.Blog.Application/ViewModels/TagViewModels/DeleteTagModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarisTutakli.Blog.Application.ViewModels.TagViewModels
+{
+    public class DeleteTagModel
+    {
+        public int Id { get; set; }
+    }
+}
