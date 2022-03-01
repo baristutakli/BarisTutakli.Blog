96ad9bb717d4d9557c06f36b96e29e0bd25422c4
kodluyoruz <baristutakli@hotmail.com>
2022-03-01 12:11:08 +0300

    PostViewModels

---
+++ src/Application/BarisTutakli.Blog.Application/ViewModels/PostViewModels/DeletePostModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BarisTutakli.Blog.Application.ViewModels.PostViewModels
+{
+    [NotMapped]
+    public class DeletePostModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Application/BarisTutakli.Blog.Application/ViewModels/PostViewModels/UpdatePostModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarisTutakli.Blog.Application.ViewModels.PostViewModels
+{
+    public class UpdatePostModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string MetaTitle { get; set; }
+        public string Summary { get; set; }
+        public bool Published { get; set; }
+        public string Body { get; set; }
+    }
+}
