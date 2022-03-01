e794981afe91c218517e0a7ad37d681d03056dd7
kodluyoruz <baristutakli@hotmail.com>
2022-03-01 19:26:06 +0300

    Added dtos

---
+++ src/Application/BarisTutakli.Blog.Application/Dto/CategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarisTutakli.Blog.Application.Dto
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ src/Application/BarisTutakli.Blog.Application/Dto/CommentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarisTutakli.Blog.Application.Dto
+{
+   public class CommentDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public UserDto User { get; set; }
+    }
+}
