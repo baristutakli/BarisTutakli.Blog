a440bad61413bcf4e9da191f2d48cbb62c2cd8d1
kodluyoruz <baristutakli@hotmail.com>
2022-03-01 12:10:10 +0300

    User Models

---
+++ src/Application/BarisTutakli.Blog.Application/ViewModels/UserViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BarisTutakli.Blog.Application.ViewModels.UserViewModels
+{
+    [NotMapped]
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8), MaxLength(20)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
