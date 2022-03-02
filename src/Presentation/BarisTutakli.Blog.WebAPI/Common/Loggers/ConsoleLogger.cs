81e42f5fd7fd755ea2b1858d3756bb914185d111
kodluyoruz <baristutakli@hotmail.com>
2022-03-02 08:47:08 +0300

    Logger

---
+++ src/Presentation/BarisTutakli.Blog.WebAPI/Common/Loggers/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarisTutakli.Blog.WebAPI.Common.Loggers
+{
+    public class ConsoleLogger : ILoggerService
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"[ConsolleLogger]: {message}");
+        }
+    }
+}
