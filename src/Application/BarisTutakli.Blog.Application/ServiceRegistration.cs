e396fda2359f82cc1ed4070878bc545a431571f4
kodluyoruz <baristutakli@hotmail.com>
2022-03-10 11:38:20 +0300

    MongoDbLogger

--- src/Application/BarisTutakli.Blog.Application/ServiceRegistration.cs
+++ src/Application/BarisTutakli.Blog.Application/ServiceRegistration.cs
@@ -1,4 +1,9 @@
-﻿using BarisTutakli.Blog.DomainServices.Interfaces;
+﻿using BarisTutakli.Blog.Application.Concrete;
+using BarisTutakli.Blog.Application.Interfaces;
+using BarisTutakli.Blog.Domain.Common;
+using BarisTutakli.Blog.Domain.Entities;
+using BarisTutakli.Blog.DomainServices.Interfaces;
+using BarisTutakli.Blog.WebAPI.Common.Loggers;
 using Blog.Application.Concrete;
 using Blog.Application.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -18,12 +23,13 @@ namespace BarisTutakli.Blog.Application
         public static void AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
             services.AddScoped<IPostService, PostService>();
             services.AddScoped<ITagService, TagService>();
             services.AddScoped<ICommentService, CommentService>();
            // services.AddScoped<IUserService, UserService>();
             services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddSingleton<ILoggerService<BaseEntity>, MongoDBLoggerService<BaseEntity>>();
 
         }
     }
