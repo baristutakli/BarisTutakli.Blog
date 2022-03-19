4ccd6a22a4b80866e7db98a3529a1b518a2cd211
kodluyoruz <baristutakli@hotmail.com>
2022-03-19 18:01:19 +0300

    Token Service

---
+++ src/Application/BarisTutakli.Blog.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using BarisTutakli.Blog.Application.Dto;
+using BarisTutakli.Blog.Application.ViewModels.UserViewModels;
+using BarisTutakli.Blog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarisTutakli.Blog.Application.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<Token> Create(User user);
+       // bool ValidateToken(string key, string issuer, string audience, string token);
+    }
+}
